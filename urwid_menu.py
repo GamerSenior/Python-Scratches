d05b09df78de21340752f8a2285f42946d602f8f
Giovani Garcia <giovani.abel@supportweb.com.br>
2018-09-19 12:50:56 -0300

    Primeiro commit dos rabiscos

--- urwid_menu.py
+++ urwid_menu.py
@@ -1,26 +1,31 @@
 import urwid
 
-choices = 'Start Pause Blabla'.split()
-
-def menu(title, choices):
-    body = [urwid.Text(title), urwid.Divider()]
-    for c in choices:
-        button = urwid.Button(c)
-        urwid.connect_signal(button, 'click', item_chosen, c)
-        body.append(urwid.AttrMap(button, None, focus_map='reversed'))
-    return urwid.ListBox(urwid.SimpleFocusListWalker(body))
-
-def item_chosen(button, choice):
-    response = urwid.Text(['you chose ', choise, '\n'])
-    done = urwid.Button('Ok')
-    urwid.connect_signal(done, 'click', exit_program)
-    main.original_widget = urwid.Filler(urwid.Pile([response,
-        urwid.AttrMap(done, None, focus_map='reversed')]))
-
 def exit_program(button):
     raise urwid.ExitMainLoop()
 
-main = urwid.Padding(menu('Pythons', choices), left=2, right=2)
+menu_abertura = [
+    {
+        'label': 'Conectar FTP Support',
+        'callback': None,
+    },{
+        'label': 'Sair',
+        'callback': exit_program,
+    }
+]
+menu_principal = ['Upload Projetos', 'Upload Relatórios', 'Sair']
+
+def menu_buttom(caption, callback):
+    button = urwid.Button(caption)
+    urwid.connect_signal(button, 'click', callback)
+    return urwid.AttrMap(button, None, focus_map='reversed')
+
+def menu(title, choices):
+    body = [urwid.Text(title), urwid.Divider()]
+    for item in menu_abertura:
+        body.append(menu_buttom(item['label'], item['callback']))
+    return urwid.ListBox(urwid.SimpleFocusListWalker(body))
+
+main = urwid.Padding(menu('Support Uploader BETA 0.1', menu_abertura), left=2, right=2)
 top = urwid.Overlay(main, urwid.SolidFill('\N{MEDIUM SHADE}'),
                     align='center', width=('relative', 60),
                     valign='middle', height=('relative', 60),
